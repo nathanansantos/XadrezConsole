e46c2dc1e2f0187a4acb669da95faf4998a0b3d5
Nathan Santos <nathanansantos@gmail.com>
2024-02-07 18:53:41 -0300

    Implementando as restricoes de movimento

--- XadrezConsole/Xadrez/PartidaXadrex.cs
+++ XadrezConsole/Xadrez/PartidaXadrex.cs
@@ -5,8 +5,8 @@ namespace Xadrez
      class PartidaXadrex
     {
         public Tabuleiro tab { get; private set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual { get; private set; }
         public bool terminada {  get; private set; }
 
         public PartidaXadrex()
@@ -26,6 +26,48 @@ namespace Xadrez
             tab.ColocarPeca(p, destino);
         }
 
+        public void RealizaJogada(Posicao origem, Posicao destino)
+        {
+            ExecutaMovimento(origem, destino);
+            turno++;
+            MudaJogador();
+        }
+
+        public void ValidarPosicaoDeOrigem(Posicao pos)
+        {
+            if(tab.peca(pos) == null)
+            {
+                throw new TabuleiroException("Não existe peça na posição de origem escolhida!");
+            }
+            if (jogadorAtual != tab.peca(pos).Cor)
+            {
+                throw new TabuleiroException("A peça de origem escolhida não é sua!");
+            }
+            if (!tab.peca(pos).ExisteMovimentosPossiveis())
+            {
+                throw new TabuleiroException("Não há movimentos possíveis para a peça de origem escolhida!");
+            }
+        }
+
+        public void ValidarPosicaoDeDestino(Posicao origem, Posicao destino)
+        {
+            if (!tab.peca(origem).PodeMoverPara(destino))
+            {
+                throw new TabuleiroException("Posição de destino inválida!");
+            }
+        }
+
+        private void MudaJogador()
+        {
+            if (jogadorAtual == Cor.Branco)
+            {
+                jogadorAtual = Cor.Preto;
+            }
+            else
+            {
+                jogadorAtual = Cor.Preto;
+            }
+        }
         private void ColocarPecas()
         {
             tab.ColocarPeca(new Torre(Cor.Branco, tab), new PosicaoXadrex('c', 1).ToPosicao());
